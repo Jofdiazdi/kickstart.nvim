e23687833c6270f28bc7bf445635546d30f989f7
= <jofdiazdi@gmail.com>
2025-03-17 16:43:12 -0500

    Adds more timeout

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -1,10 +1,10 @@
 return {
-  "stevearc/conform.nvim",
-  event = { "BufReadPre", "BufNewFile" },
+  'stevearc/conform.nvim',
+  event = { 'BufReadPre', 'BufNewFile' },
   config = function()
-    local conform = require("conform")
+    local conform = require 'conform'
 
-    conform.setup({
+    conform.setup {
       formatters_by_ft = {
         javascript = { 'prettier' },
         typescript = { 'prettier' },
@@ -23,22 +23,21 @@ return {
         python = { 'isort', 'black' },
         haskell = { 'ormolu' },
         go = { 'goimports', 'gofumpt' },
-        sql = { 'sqlfluff' }
-
+        sql = { 'sqlfluff' },
       },
       format_on_save = {
         lsp_fallback = true,
         async = false,
-        timeout_ms = 1000,
+        timeout_ms = 5000,
       },
-    })
+    }
 
-    vim.keymap.set({ "n", "v" }, "<leader>f", function()
-      conform.format({
+    vim.keymap.set({ 'n', 'v' }, '<leader>f', function()
+      conform.format {
         lsp_fallback = true,
         async = false,
-        timeout_ms = 1000,
-      })
-    end, { desc = "Format file or range (in visual mode)" })
+        timeout_ms = 5000,
+      }
+    end, { desc = 'Format file or range (in visual mode)' })
   end,
 }
