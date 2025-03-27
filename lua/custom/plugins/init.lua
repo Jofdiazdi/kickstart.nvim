c13e893c3e89874fa8e884e6c08ae8de9dbce9d7
= <jofdiazdi@gmail.com>
2025-03-27 10:50:50 -0500

    COnfig

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,6 +3,8 @@
 --
 -- See the kickstart.nvim README for more information
 return {
+  'mg979/vim-visual-multi',
+  'ThePrimeagen/vim-be-good',
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
   'nvim-lua/plenary.nvim', -- lua functions that many plugins use
   'christoomey/vim-tmux-navigator', -- tmux & split window navigation
