649b58305bb9da9933e575dcae3bf0460227b57c
Jose Diaz <jofdiazdi@gmail.com>
2025-06-18 16:40:56 -0500

    neo-tree

--- init.lua
+++ init.lua
@@ -5,4 +5,5 @@ require 'custom.lazy'
 
 vim.cmd.colorscheme 'catppuccin'
 -- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
+-- vim: ts=2 sts=2 sw=2 etc
+
